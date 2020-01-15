2ec04498da8c56337ad9c870882983847bfc5de4
Matthieu Mota <matthieumota@gmail.com>
2020-01-15 15:54:28 +0100

    Display pizza list and selected pizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,26 @@ interface Pizza {
   price: number;
 }
 
+const PIZZAS: Pizza[] = [
+  { id: 1, name: 'Reine', price: 12 },
+  { id: 2, name: '4 fromages', price: 13 },
+  { id: 3, name: 'Orientale', price: 11 },
+  { id: 4, name: 'Cannibale', price: 9 }
+];
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Pizza Party';
-  pizza: Pizza = {
-    id: 1,
-    name: 'Reine',
-    price: 12
+  selectedPizza: Pizza;
+  pizzas: Pizza[] = PIZZAS;
+
+  onSelect(pizza: Pizza): void {
+    // On récupère la pizza cliquée
+    console.log(pizza);
+    this.selectedPizza = pizza;
   }
 }
