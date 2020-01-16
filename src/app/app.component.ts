bb07a3dbf939876b5617253251714278e1a9dfdf
Matthieu Mota <matthieumota@gmail.com>
2020-01-16 11:13:19 +0100

    Create author component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Pizza } from './models/pizza.model';
+import { User } from './models/user.model';
 
 const PIZZAS: Pizza[] = [
   { id: 1, name: 'Reine', price: 12 },
@@ -17,6 +18,26 @@ export class AppComponent {
   title = 'Pizza Party';
   selectedPizza: Pizza;
   pizzas: Pizza[] = PIZZAS;
+  user: User = {
+    name: 'Mota',
+    firstname: 'Matthieu',
+    birthday: '1991-11-18',
+    avatar: 'https://www.gravatar.com/avatar/5355a3882df0fdd7689f8b0b5dc50720',
+    age: null
+  };
+
+  constructor() {
+    this.calculateAge('1991-11-18');
+  }
+
+  private calculateAge(date: string): void {
+    let currentDate = Date.now(); // 2020-01-16 en timestamp (millisecondes)
+    let birthDate = (new Date(this.user.birthday)).getTime(); // 1991-11-18 en timestamp (millisecondes)
+    let timeDiff = currentDate - birthDate;
+    let age = (new Date(timeDiff)).getFullYear() - 1970;
+
+    this.user.age = age;
+  }
 
   onSelect(pizza: Pizza): void {
     // On récupère la pizza cliquée
