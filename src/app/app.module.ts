59e38f798e3baa318c9dfa16e1f0184509911d9e
Matthieu Mota <matthieumota@gmail.com>
2020-01-17 10:34:55 +0100

    Pipe demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { PizzaComponent } from './pizza/pizza.component';
 import { AuthorComponent } from './author/author.component';
 import { IngredientListComponent } from './ingredient-list/ingredient-list.component';
 import { MenuComponent } from './menu/menu.component';
+import { TestPipe } from './test.pipe';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MenuComponent } from './menu/menu.component';
     PizzaComponent,
     AuthorComponent,
     IngredientListComponent,
-    MenuComponent
+    MenuComponent,
+    TestPipe
   ],
   imports: [
     BrowserModule,
