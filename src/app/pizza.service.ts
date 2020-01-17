3e82647acc23a01f3c24702e2cbd8d165d49a6d2
Matthieu Mota <matthieumota@gmail.com>
2020-01-17 17:25:24 +0100

    get pizza single on api

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -8,10 +8,17 @@ import { HttpClient } from '@angular/common/http';
 export class PizzaService {
   constructor(private http: HttpClient) { }
 
-  // Appel AJAX sur l'api
+  // Appel AJAX sur l'api pour TOUTES les pizzas
   getPizzas(): Promise<Pizza[]> {
     return this.http.get('http://localhost:3000/pizzas').toPromise().then(
       response => response as Pizza[]
+    );
+  }
+
+  // Récupèrer une SEULE pizza
+  getPizza(id): Promise<Pizza> {
+    return this.http.get('http://localhost:3000/pizzas/'+id).toPromise().then(
+      response => response as Pizza
     );
   }
 }
